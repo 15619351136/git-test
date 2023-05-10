82e0457f42df783735b2ddcd992fbb208a60cbce
xiaohu <960816752@qq.com>
2023-05-10 23:35:46 +0800

    master commit2

--- src/main/java/com/xiaohu/git/GitTest.java
+++ src/main/java/com/xiaohu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello world!");
         System.out.println("hello world0111111!");
         System.out.println("hello world6666!");
+        System.out.println("master test");
     }
 }
